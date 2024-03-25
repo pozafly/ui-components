adddc7c068e5c43685979a1e24bbd558f6c17c1e
pozafly <pozafly@gmail.com>
2024-03-25 22:50:48 +0900

    route wip

--- src/app/[...item]/page.tsx
+++ src/app/[...item]/page.tsx
@@ -1,15 +1,10 @@
 'use client';
 
-import { ROUTE_PATH, isParentRoute, routePaths, routes } from '@/routes';
+// import { ROUTE_PATH, isParentRoute, routePaths, routes } from '@/routes';
 
 const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
-  const path = ['', ...item].join('/') as ROUTE_PATH;
-  const route = routes[path];
-  if (!routePaths.includes(path) || isParentRoute(route) || !route.children)
-    return null;
-
-  const Component = route.children;
-  return <Component />;
+  const path = ['', ...item].join('/');
+  return <div>Item page {path}</div>;
 };
 
 export default ItemPage;

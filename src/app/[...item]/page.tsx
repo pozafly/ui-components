9737f789991f657bfd149b0ed75469d8285ac2b8
pozafly <pozafly@gmail.com>
2024-03-26 22:11:15 +0900

    route complete

--- src/app/[...item]/page.tsx
+++ src/app/[...item]/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import { ROUTE_PATH, isParentRoute, routePaths, routes } from '@/routes';
+
 const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
-  const path = ['', ...item].join('/');
-  return <div>Item page {path}</div>;
+  const path = ['', ...item].join('/') as ROUTE_PATH;
+  const route = routes[path];
+  if (!routePaths.includes(path) || isParentRoute(route) || !route.children)
+    return null;
+
+  const Component = route.children;
+  return <Component />;
 };
 
 export default ItemPage;
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,35 @@
-const routePaths = [
-  '/',
-  '/test1',
-  '/test2',
-  '/test2/vanilla',
-  '/test2/react',
-] as const;
+import Accordions from './components/01_accordion';
 
+export const routePaths = [
+  '/',
+  '/accordion',
+  // '/tabMenu',
+  // '/tooltip',
+  // '/textBox',
+  // '/lineClamp',
+  // '/lazyLoading',
+  // '/lazyLoading/1_r',
+  // '/lazyLoading/2_v',
+  // '/lazyLoading/3_r',
+  // '/infiniteScroll',
+  // '/infiniteScroll/react',
+  // '/infiniteScroll/vanilla',
+  // '/scrollBox',
+  // '/scrollSpy',
+  // '/scrollSpy/1_r',
+  // '/scrollSpy/2_r',
+  // '/scrollSpy/3_v',
+  // '/scrollSpy/4_r',
+  // '/snackbar',
+  // '/modal',
+  // '/popover',
+  // '/imageSlide',
+  // '/carousel',
+  // '/gallery',
+  // '/selectBox',
+  // '/autoComplete',
+  // '/dnd',
+] as const;
 export type ROUTE_PATH = (typeof routePaths)[number];
 
 type BaseRoute = {
@@ -19,7 +43,6 @@ export type ParentRoute = BaseRoute & {
 export type ChildRoute = BaseRoute & {
   children: ((props: unknown) => JSX.Element) | null;
 };
-
 export type ROUTE = ParentRoute | ChildRoute;
 
 export const routes: Record<ROUTE_PATH, ROUTE> = {
@@ -27,36 +50,198 @@ export const routes: Record<ROUTE_PATH, ROUTE> = {
     key: '/',
     link: '/',
     name: 'root',
-    children: ['/test1', '/test2'],
+    children: [
+      '/accordion',
+      // '/tabMenu',
+      // '/tooltip',
+      // '/textBox',
+      // '/lineClamp',
+      // '/lazyLoading',
+      // '/infiniteScroll',
+      // '/scrollBox',
+      // '/scrollSpy',
+      // '/snackbar',
+      // '/modal',
+      // '/popover',
+      // '/imageSlide',
+      // '/carousel',
+      // '/gallery',
+      // '/selectBox',
+      // '/autoComplete',
+      // '/dnd',
+    ],
   },
-  '/test1': {
-    key: '/test1',
-    link: '/test1',
-    name: '테스트1',
-    children: null,
+  '/accordion': {
+    key: '/accordion',
+    link: '/accordion',
+    name: '01. 아코디언',
+    children: Accordions,
   },
-  '/test2': {
-    key: '/test2',
-    link: '/test2',
-    name: '테스트2',
-    children: ['/test2/vanilla', '/test2/react'],
-  },
-  '/test2/vanilla': {
-    key: '/test2/vanilla',
-    link: '/test2/vanilla',
-    name: 'Vanilla',
-    children: null,
-  },
-  '/test2/react': {
-    key: '/test2/react',
-    link: '/test2/react',
-    name: 'React',
-    children: null,
-  },
+  // '/tabMenu': {
+  //   key: '/tabMenu',
+  //   link: '/tabMenu',
+  //   name: '02. 탭메뉴',
+  //   children: TabMenus,
+  // },
+  // '/tooltip': {
+  //   key: '/tooltip',
+  //   link: '/tooltip',
+  //   name: '03. 툴팁',
+  //   children: Tooltips,
+  // },
+  // '/textBox': {
+  //   key: '/textBox',
+  //   link: '/textBox',
+  //   name: '04. 반응형 텍스트박스',
+  //   children: TextBoxes,
+  // },
+  // '/lineClamp': {
+  //   key: '/lineClamp',
+  //   link: '/lineClamp',
+  //   name: '05. 여러줄 말줄임',
+  //   children: LineClamps,
+  // },
+  // '/lazyLoading': {
+  //   key: '/lazyLoading',
+  //   link: '/lazyLoading/1_r',
+  //   name: '06. 지연 로딩',
+  //   children: ['/lazyLoading/1_r', '/lazyLoading/2_v', '/lazyLoading/3_r'],
+  // },
+  // '/lazyLoading/1_r': {
+  //   key: '/lazyLoading/1_r',
+  //   link: '/lazyLoading/1_r',
+  //   name: 'React1',
+  //   children: LazyLoad1,
+  // },
+  // '/lazyLoading/2_v': {
+  //   key: '/lazyLoading/2_v',
+  //   link: '/lazyLoading/2_v',
+  //   name: 'Vanilla',
+  //   children: LazyLoad2V,
+  // },
+  // '/lazyLoading/3_r': {
+  //   key: '/lazyLoading/3_r',
+  //   link: '/lazyLoading/3_r',
+  //   name: 'React3',
+  //   children: LazyLoad3,
+  // },
+  // '/infiniteScroll': {
+  //   key: '/infiniteScroll',
+  //   link: '/infiniteScroll/react',
+  //   name: '07. 무한 스크롤',
+  //   children: ['/infiniteScroll/react', '/infiniteScroll/vanilla'],
+  // },
+  // '/infiniteScroll/react': {
+  //   key: '/infiniteScroll/react',
+  //   link: '/infiniteScroll/react',
+  //   name: 'React#1 - IO',
+  //   children: InfiniteScrollR,
+  // },
+  // '/infiniteScroll/vanilla': {
+  //   key: '/infiniteScroll/vanilla',
+  //   link: '/infiniteScroll/vanilla',
+  //   name: 'Vanilla',
+  //   children: InfiniteScrollV,
+  // },
+  // '/scrollBox': {
+  //   key: '/scrollBox',
+  //   link: '/scrollBox',
+  //   name: '08. 횡 스크롤 박스',
+  //   children: ScrollBox,
+  // },
+  // '/scrollSpy': {
+  //   key: '/scrollSpy',
+  //   link: '/scrollSpy/1_r',
+  //   name: '09. 스크롤 스파이',
+  //   children: [
+  //     '/scrollSpy/1_r',
+  //     '/scrollSpy/2_r',
+  //     '/scrollSpy/3_v',
+  //     '/scrollSpy/4_r',
+  //   ],
+  // },
+  // '/scrollSpy/1_r': {
+  //   key: '/scrollSpy/1_r',
+  //   link: '/scrollSpy/1_r',
+  //   name: 'R - scroll',
+  //   children: ScrollSpy1,
+  // },
+  // '/scrollSpy/2_r': {
+  //   key: '/scrollSpy/2_r',
+  //   link: '/scrollSpy/2_r',
+  //   name: 'R - IO',
+  //   children: ScrollSpy2,
+  // },
+  // '/scrollSpy/3_v': {
+  //   key: '/scrollSpy/3_v',
+  //   link: '/scrollSpy/3_v',
+  //   name: 'Vanilla',
+  //   children: ScrollSpy3V,
+  // },
+  // '/scrollSpy/4_r': {
+  //   key: '/scrollSpy/4_r',
+  //   link: '/scrollSpy/4_r',
+  //   name: 'R - ScrollBox',
+  //   children: ScrollSpy4,
+  // },
+  // '/snackbar': {
+  //   key: '/snackbar',
+  //   link: '/snackbar',
+  //   name: '10. 스낵바',
+  //   children: null,
+  // },
+  // '/modal': {
+  //   key: '/modal',
+  //   link: '/modal',
+  //   name: '11. 모달',
+  //   children: null,
+  // },
+  // '/popover': {
+  //   key: '/popover',
+  //   link: '/popover',
+  //   name: '12. 팝오버',
+  //   children: null,
+  // },
+  // '/imageSlide': {
+  //   key: '/imageSlide',
+  //   link: '/imageSlide',
+  //   name: '13. 이미지 슬라이드',
+  //   children: null,
+  // },
+  // '/carousel': {
+  //   key: '/carousel',
+  //   link: '/carousel',
+  //   name: '14. 캐러셀',
+  //   children: null,
+  // },
+  // '/gallery': {
+  //   key: '/gallery',
+  //   link: '/gallery',
+  //   name: '15. 갤러리',
+  //   children: null,
+  // },
+  // '/selectBox': {
+  //   key: '/selectBox',
+  //   link: '/selectBox',
+  //   name: '16. 셀렉트 박스',
+  //   children: null,
+  // },
+  // '/autoComplete': {
+  //   key: '/autoComplete',
+  //   link: '/autoComplete',
+  //   name: '17. 자동 완성',
+  //   children: null,
+  // },
+  // '/dnd': {
+  //   key: '/dnd',
+  //   link: '/dnd',
+  //   name: '18. D&D 리스트',
+  //   children: null,
+  // },
 };
 
 export const isParentRoute = (route: ROUTE): route is ParentRoute =>
-  Array.isArray(route);
+  Array.isArray(route.children);
 
 export const gnbRootList = (routes['/'] as ParentRoute).children.map(
   (r) => routes[r]
